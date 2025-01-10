270b1245d1b7f57beba1a0ee825dba66b8d5baf5
Mohosin <mohosinali075@gmail.com>
2025-01-10 15:12:41 +0600

    refactor redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client";
 
-const HomePage = async () => {
+import { useGetUsersQuery } from "@/redux/features/auth/authApi";
+
+
+
+const HomePage = () => {
+  const { data, isLoading } = useGetUsersQuery("");
+  console.log(data);
   return (
     <>
       <div>Home page</div>
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,49 +1,22 @@
-import {
-  BaseQueryApi,
-  BaseQueryFn,
-  DefinitionType,
-  FetchArgs,
-  createApi,
-  fetchBaseQuery,
-} from "@reduxjs/toolkit/query/react";
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { logout, setUser } from "../features/auth/authSlice";
-// import { toast } from 'sonner';
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
-  credentials: "include",
-  prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token;
-
-    if (token) {
-      headers.set("authorization", `${token}`);
-    }
-
-    return headers;
-  },
-});
-
-const baseQueryWithRefreshToken: BaseQueryFn<
-  FetchArgs,
-  BaseQueryApi,
-  DefinitionType
-> = async (args, api, extraOptions): Promise<any> => {
-  let result = await baseQuery(args, api, extraOptions);
-
-  if (result?.error?.status === 404) {
-    // toast.error(result.error.data.message);
-  }
-  if (result?.error?.status === 403) {
-    // toast.error(result.error.data.message);
-  }
-
-  return result;
-};
-
+// Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: baseQueryWithRefreshToken,
-  tagTypes: ["semester", "courses", "offeredCourse"],
-  endpoints: () => ({}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api-fresh-harvest.code-commando.com/api/v1",
+    // credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set("authorization", `${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({}),
 });
