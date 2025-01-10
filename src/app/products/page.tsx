328be0fe030024a79081b9ba58bdd5bbdff81991
Mohosin <mohosinali075@gmail.com>
2025-01-11 04:27:42 +0600

    shop/products page design and data load

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,61 @@
-function Shop() {
+"use client"
+import SectionTitle from "@/components/shared/SectionTitle";
+import ProductCard from "@/components/ui/ProductCard";
+import { useGetCategoriesQuery, useGetProductQuery } from "@/redux/api/baseApi";
+import Link from "next/link";
+import { useState } from "react";
+
+function ProductsPage() {
+  const { data: products, isLoading } = useGetProductQuery("");
+  const { data: categories } = useGetCategoriesQuery("");
+  const [activeTab, setActiveTab] = useState("");
+
+  const handleTabClick = (categoryName: string) => {
+    setActiveTab(categoryName);
+  };
   return (
-    <div>Shop</div>
-  )
+    <div className="my-10 container-space ">
+      <SectionTitle
+        title="Our Fresh Products"
+        description="We pride ourselves on offering a wide variety of fresh and flavorful fruits, vegetables, and salad ingredients."
+        subtitle="Our Product"
+      />
+      {/* tabs */}
+      <div className="md:w-6/12 mx-auto mt-6 px-4">
+        <div role="tablist" className="tabs tabs-lifted">
+          <a
+            role="tab"
+            className={`tab ${
+              activeTab === "" ? "tab-active bg-secondary" : ""
+            }`}
+            onClick={() => handleTabClick("")}
+          >
+            All
+          </a>
+          {categories?.data?.map((cat) => (
+            <a
+              key={cat.id}
+              role="tab"
+              className={`tab ${
+                activeTab === cat.categoryName ? "tab-active" : ""
+              }`}
+              onClick={() => handleTabClick(cat.categoryName)}
+            >
+              {cat.categoryName}
+            </a>
+          ))}
+        </div>
+      </div>
+      {/* products */}
+      <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-5 my-5">
+        {products?.data?.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div>
+      </div>
+    </div>
+  );
 }
 
-export default Shop
+export default ProductsPage;
